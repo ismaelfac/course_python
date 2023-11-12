eea45584d0eae633308fab08a0863c35c4de32fe
ISMAEL ENRIQUE LASTRE ALVAREZ <ismaelfac@gmail.com>
2023-11-12 07:21:16 -0500

    palindromo

--- apps_test/palindromo.py
+++ apps_test/palindromo.py
@@ -1,12 +1,12 @@
-def reverse(text):
+def reverse(text): 
     al_reves = ""
     for char in text:
         al_reves = char + al_reves
-    return al_reves
+    return al_reves.upper()
 
 def palindromo(texto):
     texto_sin_espacio = texto.replace(" ", "")
     texto_in_reverse = reverse(texto_sin_espacio)
     print(texto_in_reverse) 
     
-palindromo("Hola Mundo") 
+palindromo("Amo la Paloma") 

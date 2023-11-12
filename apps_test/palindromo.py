6900ef1d39172c19769b4ea62f31cf90721caa49
ISMAEL ENRIQUE LASTRE ALVAREZ <ismaelfac@gmail.com>
2023-11-12 07:10:16 -0500

    palindromo

---
+++ apps_test/palindromo.py
@@ -0,0 +1,12 @@
+def reverse(text):
+    al_reves = ""
+    for char in text:
+        al_reves = char + al_reves
+    return al_reves
+
+def palindromo(texto):
+    texto_sin_espacio = texto.replace(" ", "")
+    texto_in_reverse = reverse(texto_sin_espacio)
+    print(texto_in_reverse) 
+    
+palindromo("Hola Mundo") 
